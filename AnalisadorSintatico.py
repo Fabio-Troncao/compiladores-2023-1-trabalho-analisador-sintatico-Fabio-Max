aadf0367ac79d287809d0cedbad683dc140394cc
Fábio Troncão <fabiotroncao@gmail.com>
2023-06-07 10:55:02 -0300

    Cottecoes

--- AnalisadorSintatico.py
+++ AnalisadorSintatico.py
@@ -1,32 +1,32 @@
 from AnalisadorLexico import AnalisadorLexico, Token
 
 class ErroSintaticoException(Exception):
-    def __init__(self, obtida, *esperadas):
-        self.obtida = obtida
-        self.esperadas = esperadas
+    def __init__(self, token_atual, classe_esperada=None):
+        self.token_atual = token_atual
+        self.classe_esperada = classe_esperada
 
     def __str__(self):
-        esperadas_str = ', '.join(str(classe) for classe in self.esperadas)
-        return f"Classes esperadas: [{esperadas_str}], obtida: {self.obtida}"
-
+        if self.classe_esperada is not None:
+            return f"Erro sintático: Classe esperada: {self.classe_esperada}, obtida: {self.token_atual}"
+        else:
+            return f"Erro sintático: Token inesperado: {self.token_atual}"
 
 class AnalisadorSintatico:
     def __init__(self, analisador_lexico):
         self.analisador_lexico = analisador_lexico
-        self.token_atual = self.analisador_lexico.proximo_token()
+        self.proximo_token()
 
     def proximo_token(self):
         self.token_atual = self.analisador_lexico.proximo_token()
-        return self.token_atual
 
     def analisar(self):
         self.program()
-
+    
     def program(self):
-        while self.token_atual.valor != "EOF":
+        while self.token_atual and self.token_atual.valor != "EOF":
             self.declaration()
-            # self.proximo_token()
-
+            
+            
     def declaration(self):
         if self.token_atual.tipo == "Palavra reservada" and self.token_atual.valor == "fun":
             self.funDecl()
@@ -42,42 +42,44 @@ class AnalisadorSintatico:
     def varDecl(self):
         self.checarToken("Palavra reservada", "var")
         self.checarToken("Identificador", self.token_atual.valor)
-        self.checarToken("Operador", "=")
-        self.expression()
+        
+        if self.checarToken("Operador", "="):
+            self.expression()
+        
         self.checarToken("Delimitador", ";")
 
     def statement(self):
-        if self.token_atual.tipo == "Palavra reservada" and self.token_atual.valor == "print":
-            self.printStmt()
-        elif self.token_atual.tipo == "Palavra reservada" and self.token_atual.valor == "return":
-            self.returnStmt()
-        elif self.token_atual.tipo == "Palavra reservada" and self.token_atual.valor == "if":
-            self.ifStmt()
-        elif self.token_atual.tipo == "Palavra reservada" and self.token_atual.valor == "while":
-            self.whileStmt()
-        elif self.token_atual.tipo == "Palavra reservada" and self.token_atual.valor == "for":
-            self.forStmt()
+        if self.token_atual.tipo == "Palavra reservada":
+            if self.token_atual.valor == "print":
+                self.printStmt()
+            elif self.token_atual.valor == "return":
+                self.returnStmt()
+            elif self.token_atual.valor == "if":
+                self.ifStmt()
+            elif self.token_atual.valor == "while":
+                self.whileStmt()
+            elif self.token_atual.valor == "for":
+                self.forStmt()
         elif self.token_atual.tipo == "Delimitador" and self.token_atual.valor == "{":
             self.block()
-        elif self.token_atual.tipo == "Delimitador" and self.token_atual.valor == "EOF":
-            return
         else:
             self.exprStmt()
 
     def exprStmt(self):
         self.expression()
-        # self.proximo_token()
-        self.checarToken("Delimitador", ";")
+        if self.token_atual is not None and self.token_atual.tipo == "Delimitador" and self.token_atual.valor == ";":
+            self.checarToken("Delimitador", ";")
+        else:
+            raise ErroSintaticoException(self.token_atual)
 
     def forStmt(self):
         self.checarToken("Palavra reservada", "for")
         self.checarToken("Delimitador", "(")
         if self.token_atual.tipo == "Palavra reservada" and self.token_atual.valor == "var":
             self.varDecl()
-        elif self.token_atual.tipo != "Delimitador" and self.token_atual.valor != ";":
+        elif self.token_atual.tipo != "Delimitador" or self.token_atual.valor != ";":
             self.exprStmt()
-        else:
-            self.checarToken("Delimitador", ";")
+        self.checarToken("Delimitador", ";")
         if self.token_atual.tipo != "Delimitador" and self.token_atual.valor != ";":
             self.expression()
         self.checarToken("Delimitador", ";")
@@ -92,28 +94,28 @@ class AnalisadorSintatico:
         self.expression()
         self.checarToken("Delimitador", ")")
         self.statement()
-        if self.token_atual.tipo == "Palavra reservada" and self.token_atual.valor == "else":
+
+        if self.token_atual is not None and self.token_atual.tipo == "Palavra reservada" and self.token_atual.valor == "else":
             self.checarToken("Palavra reservada", "else")
             self.statement()
 
     def printStmt(self):
         self.checarToken("Palavra reservada", "print")
         self.expression()
-        if self.token_atual.tipo == "Delimitador" and self.token_atual.valor == ";":
-            self.checarToken("Delimitador", ";")
-        else:
-            raise ErroSintaticoException(self.token_atual, "Delimitador", ";")
-
+        self.checarToken("Delimitador", ";")
+        
     def returnStmt(self):
         self.checarToken("Palavra reservada", "return")
         if self.token_atual.tipo != "Delimitador" or self.token_atual.valor != ";":
             self.expression()
         self.checarToken("Delimitador", ";")
-
+        
     def whileStmt(self):
         self.checarToken("Palavra reservada", "while")
         self.checarToken("Delimitador", "(")
         self.expression()
+        if self.token_atual.tipo != "Delimitador" or self.token_atual.valor != ")":
+            self.expression()
         self.checarToken("Delimitador", ")")
         self.statement()
 
@@ -121,42 +123,34 @@ class AnalisadorSintatico:
         self.checarToken("Delimitador", "{")
         while self.token_atual is not None and self.token_atual.valor != "}":
             self.declaration()
-            # self.proximo_token()
-
-        if self.token_atual is None:
-            self.token_esperado = ["}"]
-            raise ErroSintaticoException(None, self.token_esperado, "EOF")
-
         self.checarToken("Delimitador", "}")
 
     def expression(self):
         self.assignment()
 
-    # assignment → ( call "." )? IDENTIFIER "=" assignment | logic_or
-
     def assignment(self):
-        self.logic_or()
+        if self.token_atual.tipo == "Delimitador" and self.token_atual.valor == "(":
+            self.checarToken("Delimitador", "(")
+            self.call()
+            self.checarToken("Operador", ".")
+            self.checarToken("Identificador", self.token_atual.valor)
+            self.checarToken("Operador", "=")
+            self.assignment()
+        else:
+            self.logic_or()
 
-        if self.token_atual.tipo == "Operador" and self.token_atual.valor == "=":
+        if self.token_atual is not None and self.token_atual.tipo == "Operador" and self.token_atual.valor == "=":
             self.checarToken("Operador", "=")
             self.assignment()
 
-    # # assignment → ( call "." )? IDENTIFIER "=" assignment | logic_or
-
     # def assignment(self):
-    #     if self.token_atual.tipo == "Delimitador" and self.token_atual.valor == "(":
-    #         self.checarToken("Delimitador", "(")
-    #         self.call()
-    #         self.checarToken("Operador", ".")
+    #     self.logic_or()
     #     if self.token_atual.tipo == "Identificador":
     #         self.checarToken("Identificador", self.token_atual.valor)
-    #         if self.token_atual.tipo == "Operador" and self.token_atual.valor == "=":
-    #             self.checarToken("Operador", "=")
-    #             self.assignment()
+    #     elif self.token_atual.tipo == "Operador" and self.token_atual.valor == "=":
+    #         self.checarToken("Operador", "=")
     #         self.assignment()
-    #     else:
-    #         self.logic_or()
-
+  
     def logic_or(self):
         self.logic_and()
         while self.token_atual and self.token_atual.tipo == "Palavra reservada" and self.token_atual.valor == "or":
@@ -210,26 +204,26 @@ class AnalisadorSintatico:
             self.unary()
 
     def unary(self):
-        if  self.token_atual and self.token_atual.tipo == "Operador" and (self.token_atual.valor == "!" or self.token_atual.valor == "-"):
+        if self.token_atual and self.token_atual.tipo == "Operador":
             if self.token_atual.valor == "!":
                 self.checarToken("Operador", "!")
+                self.unary()
             elif self.token_atual.valor == "-":
                 self.checarToken("Operador", "-")
-            self.unary()
+                self.unary()
         else:
             self.call()
 
     def call(self):
         self.primary()
-
-        while self.token_atual and (self.token_atual.valor == "(" or self.token_atual.valor == "."):
-            if self.token_atual.valor == "(":
+        while self.token_atual and (self.token_atual.tipo == "Delimitador" and self.token_atual.valor == "(" or self.token_atual.tipo == "Operador" and self.token_atual.valor == "."):
+            if self.token_atual.tipo == "Delimitador" and self.token_atual.valor == "(":
                 self.checarToken("Delimitador", "(")
                 self.arguments()
                 self.checarToken("Delimitador", ")")
-            elif self.token_atual.valor == ".":
+            elif self.token_atual.tipo == "Operador" and self.token_atual.valor == ".":
                 self.checarToken("Operador", ".")
-                self.checarToken("Identificador")
+                self.checarToken("Identificador", self.token_atual.valor)
 
     def primary(self):
         if self.token_atual is None:
@@ -237,10 +231,10 @@ class AnalisadorSintatico:
 
         if self.token_atual.valor in ["true", "false", "nil", "this"]:
             self.checarToken("Palavra reservada", self.token_atual.valor)
-        elif self.token_atual.tipo in ["Inteiro", "Ponto Flutuante", "Constante Textual", "Identificador", "Operador","Delimitador"]:
+        elif self.token_atual.tipo in ["Inteiro", "Ponto Flutuante", "Constante Textual", "Identificador", "Delimitador", "Operador"]:
             self.checarToken(self.token_atual.tipo, self.token_atual.valor)
         elif self.token_atual.valor == "(":
-            self.checarToken("Delimitador", "(")    
+            self.checarToken("Delimitador", "(")
             self.expression()
             self.checarToken("Delimitador", ")")
         elif self.token_atual.valor == "super":
@@ -248,8 +242,7 @@ class AnalisadorSintatico:
             self.checarToken("Delimitador", ".")
             self.checarToken("Identificador", self.token_atual.valor)
         else:
-            raise ErroSintaticoException(
-                self.token_atual, self.analisador_lexico.obter_token_atual())
+            raise ErroSintaticoException(self.token_atual, "Token inválido")
 
     def function(self):
         if self.token_atual.tipo != "Identificador":
@@ -265,9 +258,16 @@ class AnalisadorSintatico:
     def parameters(self):
         if self.token_atual.tipo == "Identificador":
             self.checarToken("Identificador", self.token_atual.valor)
+            
             while self.token_atual.tipo == "Delimitador" and self.token_atual.valor == ",":
                 self.checarToken("Delimitador", ",")
-                self.checarToken("Identificador", self.token_atual.valor)
+                
+                if self.token_atual.tipo == "Identificador":
+                    self.checarToken("Identificador", self.token_atual.valor)
+                else:
+                    raise ErroSintaticoException(self.token_atual, "Identificador")             
+        else:
+            raise ErroSintaticoException(self.token_atual, "Identificador")
 
     def arguments(self):
         self.expression()
@@ -275,16 +275,29 @@ class AnalisadorSintatico:
             self.checarToken("Delimitador", ",")
             self.expression()
 
-    def checarToken(self, tipo_esperado, valor_esperado=None):
+    def checarToken(self, tipo_esperado, classe_esperada=None):
         if self.token_atual is None:
             raise ErroSintaticoException("Fim do arquivo inesperado")
 
-        if self.token_atual.tipo == tipo_esperado and str(self.token_atual.valor) == valor_esperado:
+        if self.token_atual.tipo == tipo_esperado and (classe_esperada is None or self.token_atual.valor == classe_esperada):
             print("Token processado:", self.token_atual)
-            self.token_atual = self.proximo_token()
+            self.proximo_token()
             return True
         else:
-            raise ErroSintaticoException(self.token_atual, tipo_esperado, valor_esperado)
+            raise ErroSintaticoException(self.token_atual, classe_esperada)
+
+
+#     def executar_analise_sintatica(self):
+#         try:
+#             self.analisar()
+#             print("Análise sintática concluída sem erros.")
+#         except ErroSintaticoException as e:
+#             print("Erro sintático:", e)
+
+
+# analisador_lexico = AnalisadorLexico("teste.c")
+# analisador_sintatico = AnalisadorSintatico(analisador_lexico)
+# analisador_sintatico.executar_analise_sintatica()
 
 if __name__ == '__main__':
     print("------------ Analisador Sintático ------------")
--- erro_sintatico.py
+++
@@ -1,8 +0,0 @@
-class ErroSintaticoException(Exception):
-    def __init__(self, obtida, *esperadas):
-        self.obtida = obtida
-        self.esperadas = esperadas
-
-    def __str__(self):
-        esperadas_str = ', '.join(str(classe) for classe in self.esperadas)
-        return f"Classes esperadas: [{esperadas_str}], obtida: {self.obtida}"
